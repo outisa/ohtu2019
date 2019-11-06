04f09447f91994f724649e6c3dde75a48544ff2c
outisa <savolainen.outi@gmail.com>
2019-11-06 12:18:46 +0200

    muutoksia verkkokauppaan

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/KauppaInterface.java
@@ -0,0 +1,15 @@
+
+package ohtu.verkkokauppa;
+
+
+public interface KauppaInterface {
+
+    void aloitaAsiointi();
+
+    void lisaaKoriin(int id);
+
+    void poistaKorista(int id);
+
+    boolean tilimaksu(String nimi, String tiliNumero);
+    
+}
